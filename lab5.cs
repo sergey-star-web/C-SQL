31ab2e833392da3fb30eaf3422b3d70161349f3a
sergey-star-web <81182274+sergey-star-web@users.noreply.github.com>
2022-03-10 16:13:30 +0300

    Update lab5.cs

--- lab5.cs
+++ lab5.cs
@@ -31,48 +31,43 @@ namespace task5
     {
         private string worker;
         public string product;
-        public string doc;
         public int qty;
         private double price;
 
         public Shop()
         {
-            Console.Write("Введите имя работника: ");
+            Console.Write("Введите имя директора: ");
             worker = Console.ReadLine();
             Console.WriteLine();
             Console.Write("Введите название продукта: ");
             product = Console.ReadLine();
             Console.WriteLine();
-            Console.Write("Введите название документа: ");
-            doc = Console.ReadLine();
-            Console.WriteLine();
-            Console.Write("Введите количество продуктов: ");
+            Console.Write("Введите количество работников: ");
             qty = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
-            Console.Write("Введите цену продукта: ");
+            Console.Write("Введите цену магазина: ");
             price = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine();
         }
 
-        public Shop(string worker, string product, string doc, int qty, double price)
+        public Shop(string worker, string product, int qty, double price)
         {
             this.worker = worker;
             this.product = product;
-            this.doc = doc;
             this.qty = qty;
             this.price = price;
         }
 
         public void Show()
         {
-            Console.WriteLine("Name worker: " + worker + " name product: " + product + " name document: " + doc +
+            Console.WriteLine("Name worker: " + worker + " name product: " + product + 
                 " quantity products: " + qty.ToString() + " Price product: " + price.ToString());
         }
 
         public static Shop operator +(Shop s1, Shop s2)
         {
            
-            return new Shop(s1.worker + " " + s2.worker, s1.product + " " + s2.product, s1.doc + " " + s2.doc, s1.qty + s2.qty, s1.price + s2.price);
+            return new Shop(s1.worker + " " + s2.worker, s1.product + " " + s2.product, s1.qty + s2.qty, s1.price + s2.price);
            
         }
 
@@ -99,7 +94,7 @@ namespace task5
             Shop shop1 = new Shop();
             shop1.Show();
             Console.WriteLine();
-            Shop shop2 = new Shop("Jhon", "cake", "ГОСТ345", 25, 15.45);
+            Shop shop2 = new Shop("Jhon", "cake",  25, 15.45);
             shop2.Show();
             Console.WriteLine();
             Shop shop3 = new Shop();
